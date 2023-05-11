253a1a788362b347079cf5c251fd2a17a80ac502
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-05-11 09:48:32 +0200

    Link to Github

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <a href="https://github.com/FidelStucht/weather-react.">Git Hub Link</a>
   </React.StrictMode>
 );
 
